1ec632acffb36935251549f9518c4c1e087b0a3f
Str1XHyper <50529564+Str1XHyper@users.noreply.github.com>
2020-10-12 10:56:20 +0200

    - Added Team views - Create Team view working           -> Handling still needs work

---
+++ F4DEDTournaments/Controllers/TeamController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using F4DEDTournaments.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace F4DEDTournaments.Controllers
+{
+    public class TeamController : Controller
+    {
+        [HttpGet]
+        public IActionResult CreateTeam()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CreateTeam(CreateTeamViewModel model)
+        {
+
+
+            return View();
+        }
+    }
+}
---
+++ F4DEDTournaments/Models/Team/CreateTeamViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace F4DEDTournaments.Models
+{
+    public enum Games
+    {
+        CounterStrike = 0,
+        CallOfDuty = 1,
+        RainbowSixSiege = 2,
+        Valorant = 3,
+    }
+
+    public class CreateTeamViewModel
+    {
+        [Required]
+        public string TeamName { get; set; }
+        [Required]
+        public int MinimumElo { get; set; }
+        [Required]
+        public bool IsPrivate { get; set; }
+        public string Description { get; set; }
+        [Required]
+        public Countries Country { get; set; }
+        [Required]
+        public Languages Language { get; set; }
+        [Required]
+        public int MinimumAge { get; set; }
+        [Required]
+        public Games PlayedGame { get; set; }
+    }
+}
